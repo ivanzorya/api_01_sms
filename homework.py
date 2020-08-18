3899ddfc9d3442c51bcb42fc98cd484da2feb15f
Ivan Zorya <ivanzorya@gmail.com>
2020-08-18 12:52:34 +0300

    second try v1

--- homework.py
+++ homework.py
@@ -5,21 +5,30 @@ from twilio.rest import Client
 from dotenv import load_dotenv
 import os
 import logging
+
+
 load_dotenv()
+VK_API_VERSION = os.getenv('VK_API_VERSION')
+VK_TOKEN = os.getenv('VK_TOKEN')
+ACCOUNT_SID = os.getenv('ACCOUNT_SID ')
+AUTH_TOKEN = os.getenv('AUTH_TOKEN')
+NUMBER_FROM = os.getenv('NUMBER_FROM')
+NUMBER_TO = os.getenv('NUMBER_TO')
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
+URL = 'https://api.vk.com/method/'
+USER_GET = 'users.get'
 
 
 def get_status(user_id):
-    VK_API_VERSION = os.getenv('VK_API_VERSION')
-    VK_TOKEN = os.getenv('VK_TOKEN')
     params = {
         'user_ids': user_id,
         'fields': 'online',
         'v': VK_API_VERSION,
         'access_token': VK_TOKEN
     }
-    URL = 'https://api.vk.com/method/users.get'
+    url = URL + USER_GET
     try:
-        request = requests.post(URL, params=params).json()['response']
+        request = requests.post(url, params=params).json()['response']
         user_status = request[0]['online']
         return user_status
     except Exception:
@@ -27,11 +36,6 @@ def get_status(user_id):
 
 
 def sms_sender(sms_text):
-    ACCOUNT_SID = os.getenv('ACCOUNT_SID ')
-    AUTH_TOKEN = os.getenv('AUTH_TOKEN')
-    NUMBER_FROM = os.getenv('NUMBER_FROM')
-    NUMBER_TO = os.getenv('NUMBER_TO')
-    client = Client(ACCOUNT_SID, AUTH_TOKEN)
     message = client.messages.create(
         body=sms_text,
         from_=NUMBER_FROM,
